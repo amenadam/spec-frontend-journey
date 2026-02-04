204a75a664eb67a0857f63c161ece36136bfaa45
Amenadam Solomon <aminadamsolomon5@gmail.com>
2026-02-04 17:32:14 +0300

    Learnt about reactivity. v-model and v-if

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const name = ref("");
+</script>
 
 <template>
-  <h1>Hello SPEC Developers</h1>
+  <div
+    class="w-screen h-screen flex flex-col items-center justify-center bg-black text-white"
+  >
+    <h1
+      v-if="name"
+      class="font-semibold animate-pulse hover:-translate-y-1 hover:tracking-wider cursor-pointer transition-all"
+    >
+      Hello {{ name }}, How are you?
+    </h1>
+    <input
+      type="text"
+      placeholder="enter your name"
+      v-model="name"
+      class="border border-white/20 rounded px-3 py-2 mt-3"
+    />
+  </div>
 </template>
 
 <style scoped></style>
