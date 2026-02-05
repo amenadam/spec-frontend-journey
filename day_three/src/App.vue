92696d31dfe4ec5c475f7a825ebb901e82fdde5c
Amenadam Solomon <aminadamsolomon5@gmail.com>
2026-02-05 17:33:49 +0300

    Learnt about components and how to use them

---
+++ day_three/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import UserCard from "./components/UserCard.vue";
+const users = [
+  { name: "Abebe ", role: "Team Leader" },
+  { name: "Solomon ", role: "Finance Officer" },
+  { name: "Kebede ", role: "CTO" },
+];
+</script>
+
+<template>
+  <div
+    class="w-screen h-screen flex flex-col items-center justify-center bg-black text-white"
+  >
+    <h1
+      class="text-2xl font-semibold animate-pulse hover:-translate-y-1 hover:tracking-wider cursor-pointer transition-all"
+    >
+      Users
+    </h1>
+    <div class="flex gap-3 mt-3 flex-col md:flex-row">
+      <UserCard v-for="user in users" :name="user.name" :role="user.role" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ day_three/src/components/UserCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { User } from "lucide-vue-next";
+
+defineProps(["name", "role"]);
+</script>
+<template>
+  <div
+    class="flex gap-3 bg-white/5 px-4 md:px-8 py-5 rounded-2xl items-center hover:bg-white/10 cursor-pointer"
+  >
+    <User class="text-white" size="40px" />
+    <div class="flex flex-col gap-0">
+      <h1 class="">
+        <span class="font-thin text-sm"> Name: </span>
+        <strong>{{ name }}</strong>
+      </h1>
+      <p class="text-xs font-light text-white/50">{{ role }}</p>
+    </div>
+  </div>
+</template>
