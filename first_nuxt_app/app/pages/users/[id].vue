6354bcc1083f2f3c2ef3e1d70467a417a73dfe81
Amenadam Solomon <aminadamsolomon5@gmail.com>
2026-02-11 19:50:02 +0300

    Or with out transform method

--- first_nuxt_app/app/pages/users/[id].vue
+++ first_nuxt_app/app/pages/users/[id].vue
@@ -1,13 +1,18 @@
 <script setup>
 const route = useRoute();
 const id = route.params.id;
-
+/*
 const { data, error, status } = await useFetch(
   `https://spec-frontend-journey.vercel.app/users/${id}`,
   {
     transform: (res) => res.data,
   },
+);*/
+
+const { data: user, status } = await useFetch(
+  `https://spec-frontend-journey.vercel.app/users/${id}`,
 );
+console.log(user);
 </script>
 
 <template>
@@ -22,13 +27,13 @@ const { data, error, status } = await useFetch(
       User not found
     </h1>
 
-    <div v-if="data" class="text-white bg-white/5 px-4 py-6 rounded-2xl">
+    <div class="text-white bg-white/5 px-4 py-6 rounded-2xl">
       <h1 class="font-bold text-xl">User Data</h1>
       <p>
-        User name: <strong> {{ data?.fullName }}</strong>
+        User name: <strong> {{ user?.data.fullName }}</strong>
       </p>
       <p>
-        User type: <strong>{{ data?.userType }}</strong>
+        User type: <strong>{{ user?.data.userType }}</strong>
       </p>
     </div>
 
